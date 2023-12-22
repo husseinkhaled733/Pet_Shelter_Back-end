774a65ae3ef8b9d7bb0cc1ec56fae206f05bec38
husseinkhaled733 <husseinkhaled733@gmail.com>
2023-12-22 20:31:33 +0200

    initial commit

---
+++ src/test/java/com/example/Pet_Shelter/PetShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Pet_Shelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShelterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
